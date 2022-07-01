0e5f117232e5e50b678040b7ee26bff0994cf58d
umenokoudai <umenokoudai0805@gmail.com>
2022-07-02 00:41:40 +0900

    移動方向にキャラを向ける

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     Vector2 _playerPower;
     public float _moveSpeed = 5f;
     float _horizontal;
+    float _time;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,17 +19,47 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float x = Input.GetAxis("Horizontal");
+        //float y = Input.GetAxis("Vertical");
+
+        //_playerMove.velocity = new Vector2(x + _moveSpeed, y + _moveSpeed);
+        FlipX(x);
         if (Input.GetKey(KeyCode.A))
         {
             _playerMove.velocity = new Vector2(-_moveSpeed, 0);
+            //StartCoroutine(Click());
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            _playerMove.velocity = new Vector2(_moveSpeed,0);
+            _playerMove.velocity = new Vector2(_moveSpeed, 0);
+            //StartCoroutine(Click());
         }
         if (_horizontal < 0)
         {
             _playerMove.velocity = Vector2.zero;
         }
+        void FlipX(float horizontal)
+        {
+            if(horizontal > 0)
+            {
+                transform.localScale = new Vector2(1, transform.localScale.y);
+            }
+            else if(horizontal < 0)
+            {
+                transform.localScale = new Vector2(-1, transform.localScale.y);
+            }
+        }
     }
-}
+
+    //IEnumerator Click()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.A))
+    //    {
+    //        _playerMove.velocity = new Vector2(-_moveSpeed * 10, 0);
+    //    }
+    //    else if (Input.GetKeyDown(KeyCode.D))
+    //    {
+    //        _playerMove.velocity = new Vector2(_moveSpeed * 10, 0);
+    //    }
+    //    yield return new WaitForSecondsRealtime(0.5f);
+    }
